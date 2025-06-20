668931b3c6571e2beb5650318c5cd6fb4cdda6c2
Yaminipradeepthi <161411932+Yaminipradeepthi@users.noreply.github.com>
2025-06-20 10:21:36 +0000

    Update EmployeeEntity.cs

--- Microsoft.ILP2025.EmployeeCRUD.Entities/EmployeeEntity.cs
+++ Microsoft.ILP2025.EmployeeCRUD.Entities/EmployeeEntity.cs
@@ -17,12 +17,4 @@
 
 
 
-// namespace Microsoft.ILP2025.EmployeeCRUD.Entities
-// {
-//     public class EmployeeEntity
-//     {
-//         public int Id { get; set; }
 
-//         public string Name { get; set; }        
-//     }
-// }

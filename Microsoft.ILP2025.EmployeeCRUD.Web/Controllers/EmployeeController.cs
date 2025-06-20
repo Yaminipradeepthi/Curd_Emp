b6ce6bbf4109946d6762e580190a43e9f0828bdf
Yaminipradeepthi <161411932+Yaminipradeepthi@users.noreply.github.com>
2025-06-20 10:22:40 +0000

    Update EmployeeController.cs

--- Microsoft.ILP2025.EmployeeCRUD.Web/Controllers/EmployeeController.cs
+++ Microsoft.ILP2025.EmployeeCRUD.Web/Controllers/EmployeeController.cs
@@ -84,25 +84,6 @@ namespace Microsoft.ILP2025.EmployeeCRUD.Web.Controllers
         
         
         
-        // public IEmployeeService employeeService { get; set; }
-
-        // public EmployeeController(IEmployeeService employeeService)
-        // {
-        //     this.employeeService = employeeService;
-        // }
-
-        // // GET: EmployeeController
-        // public async Task<ActionResult> Index()
-        // {
-        //     var employees = await this.employeeService.GetAllEmployees();
-        //     return View(employees);
-        // }
-
-        // // GET: EmployeeController/Details/5
-        // public async Task<ActionResult> Details(int id)
-        // {
-        //     var employee = await this.employeeService.GetEmployee(id);
-        //     return View(employee);
-        // }      
+       
     }
 }
